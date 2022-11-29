3e6f3956ff4a303cb154a1eec08be7652842e51c
inamurrehman420 <78412525+inamurrehman420@users.noreply.github.com>
2022-11-29 16:39:09 +0500

    Add files via upload

---
+++ js/5662-js-theme-script.js
@@ -0,0 +1,346 @@
+/* ------------------------------------------------
+  Project:   Taypo - Multipurpose Bootstrap5 Template
+  Author:    ThemeHt
+------------------------------------------------ */
+/* ------------------------
+    Table of Contents
+
+  1. Predefined Variables
+  2. Preloader  
+  3. Counter
+  4. Owl carousel
+  5. Dropdown
+  6. Isotope
+  7. Magnific Popup
+  8. Fixed Header
+  9. Text Color, Background Color And Image
+  10. Contact Form
+  11. Parallax
+  12. Countdown
+  13. Btnproduct
+  14. Window load and functions
+  
+
+------------------------ */
+
+"use strict";
+
+/*------------------------------------
+  HT Predefined Variables
+--------------------------------------*/
+var $window = $(window),
+    $document = $(document),
+    $body = $('body');
+
+//Check if function exists
+$.fn.exists = function () {
+  return this.length > 0;
+};
+
+
+/*------------------------------------
+  HT PreLoader
+--------------------------------------*/
+function preloader() {
+   $('#ht-preloader').fadeOut();
+};
+
+
+/*------------------------------------
+  HT Owl Carousel
+--------------------------------------*/
+function owlcarousel() {
+$('.owl-carousel').each( function() {
+  var $carousel = $(this);
+  $carousel.owlCarousel({
+      items : $carousel.data("items"),
+      slideBy : $carousel.data("slideby"),
+      center : $carousel.data("center"),
+      loop : true,
+      margin : $carousel.data("margin"),
+      dots : $carousel.data("dots"),
+      nav : $carousel.data("nav"),      
+      autoplay : $carousel.data("autoplay"),
+      autoplayTimeout : $carousel.data("autoplay-timeout"),
+      navText : [ '<span class="bi bi-arrow-left-short"><span>', '<span class="bi bi-arrow-right-short"></span>' ],
+      responsive: {
+        0:{items: $carousel.data('xs-items') ? $carousel.data('xs-items') : 1},
+        576:{items: $carousel.data('sm-items')},
+        768:{items: $carousel.data('md-items')},
+        1024:{items: $carousel.data('lg-items')},
+        1200:{items: $carousel.data("items")}
+      },
+  });
+});
+};
+
+
+/*------------------------------------
+  HT Dropdown
+--------------------------------------*/  
+function dropdown() {
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass("show");
+  });
+
+  return false;
+});
+};
+
+
+/*------------------------------------
+  HT Counter
+--------------------------------------*/
+function counter() {
+  var elementSelector = $('.count');
+    elementSelector.each(function(){
+        elementSelector.appear(function(e) {
+            var el = this;
+            var updateData = $(el).attr("data-count");
+            var od = new Odometer({
+                el: el,
+                format: 'd',
+                duration: 2000
+            });
+            od.update(updateData);
+        });
+    });
+};
+
+
+/*------------------------------------
+  HT Isotope
+--------------------------------------*/ 
+function isotope() {
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+  });
+
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+
+  // bind filter button click
+  $('.portfolio-filter').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+
+
+  // change is-checked class on buttons
+  $('.portfolio-filter').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+};
+
+
+/*------------------------------------
+  HT Magnific Popup
+--------------------------------------*/
+function magnificpopup() {
+$('.popup-gallery').magnificPopup({
+    delegate: 'a.popup-img',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+      }
+    }
+  });
+if ($(".popup-youtube, .popup-vimeo, .popup-gmaps").exists()) {
+     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+          disableOn: 700,
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: false,
+          fixedContentPos: false
+    });
+  }
+
+};     
+
+
+/*------------------------------------
+  HT Scroll to top
+--------------------------------------*/
+function scrolltop() {
+  var pxShow = 300,
+    goTopButton = $(".scroll-top")
+    // Show or hide the button
+  if ($(window).scrollTop() >= pxShow) goTopButton.addClass('scroll-visible');
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= pxShow) {
+      if (!goTopButton.hasClass('scroll-visible')) goTopButton.addClass('scroll-visible')
+    } else {
+      goTopButton.removeClass('scroll-visible')
+    }
+  });
+  $('.smoothscroll').on('click', function (e) {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 0);
+    return false;
+  });
+};
+
+
+/*------------------------------------
+  HT Fixed Header
+--------------------------------------*/
+function fxheader() {
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= 300) {
+      $('#header-wrap').addClass('fixed-header');
+    } else {
+      $('#header-wrap').removeClass('fixed-header');
+    }
+  });
+};
+
+
+/*------------------------------------------
+  HT Text Color, Background Color And Image
+---------------------------------------------*/
+function databgcolor() {
+    $('[data-bg-color]').each(function(index, el) {
+     $(el).css('background-color', $(el).data('bg-color'));  
+    });
+    $('[data-text-color]').each(function(index, el) {
+     $(el).css('color', $(el).data('text-color'));  
+    });
+    $('[data-bg-img]').each(function() {
+     $(this).css('background-image', 'url(' + $(this).data("bg-img") + ')');
+    });
+};
+
+
+/*------------------------------------
+  HT Contact Form
+--------------------------------------*/
+function contactform() { 
+
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+        var url = "php/contact.php";
+
+        // POST values in the background the the script URL
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $(this).serialize(),
+            success: function (data)
+            {
+            // data = JSON object that contact.php returns
+
+            // we recieve the type of the message: success x danger and apply it to the 
+            var messageAlert = 'alert-' + data.type;
+            var messageText = data.message;
+
+            // let's compose Bootstrap alert box HTML
+            var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+            
+            // If we have messageAlert and messageText
+            if (messageAlert && messageText) {
+                // inject the alert to .messages div in our form
+                $('#contact-form').find('.messages').html(alertBox).show().delay(2000).fadeOut('slow');
+                // empty the form
+                $('#contact-form')[0].reset();
+            }
+          }
+        });
+        return false;
+    }
+ })    
+};
+
+
+/*------------------------------------
+  HT Countdown
+--------------------------------------*/
+function countdown() {
+  $('.countdown').each(function () {
+    var $this = $(this),
+      finalDate = $(this).data('countdown');
+    $this.countdown(finalDate, function (event) {
+      $(this).html(event.strftime('<li><span>%-D</span><p>Days</p></li>' + '<li><span>%-H</span><p>Hours</p></li>' + '<li><span>%-M</span><p>Minutes</p></li>' + '<li><span>%S</span><p>Seconds</p></li>'));
+    });
+  });
+};
+
+
+/*------------------------------------
+  HT btnproduct
+--------------------------------------*/
+function btnproduct() {
+  $('.btn-product-up').on('click', function (e) {
+    e.preventDefault();
+    var numProduct = Number($(this).next().val());
+    if (numProduct > 1) $(this).next().val(numProduct - 1);
+  });
+  $('.btn-product-down').on('click', function (e) {
+    e.preventDefault();
+    var numProduct = Number($(this).prev().val());
+    $(this).prev().val(numProduct + 1);
+  }); 
+};
+
+
+/*------------------------------------
+  HT Window load and functions
+--------------------------------------*/
+$(document).ready(function() {
+    owlcarousel(),
+    dropdown(),
+    counter(),
+    magnificpopup(),
+    scrolltop(),
+    fxheader(),
+    databgcolor(),  
+    contactform(),
+    countdown(),
+    btnproduct();
+});
+
+
+$(window).on('load', function() {
+    preloader(),
+    isotope();
+});
+
